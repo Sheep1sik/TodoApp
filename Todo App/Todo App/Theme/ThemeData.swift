e8a7a43a516cb26b237ccb846511f7662b318cda
Sheep1sik <bohg6799@naver.com>
2024-04-03 15:46:27 +0900

    Feat: TodoApp 완성

---
+++ Todo App/Todo App/Theme/ThemeData.swift
@@ -0,0 +1,13 @@
+//
+//  Created by 양원식 on 2024/03/27.
+//
+
+import SwiftUI
+
+// MARK: - THEME DATA
+
+let themeData: [Theme] = [
+  Theme(id: 0, themeName: "Pink theme", themeColor: Color.pink),
+  Theme(id: 1, themeName: "Blue theme", themeColor: Color.blue),
+  Theme(id: 2, themeName: "Green theme", themeColor: Color.green)
+]
---
+++ Todo App/Todo App/Theme/ThemeModel.swift
@@ -0,0 +1,13 @@
+//
+//  Created by 양원식 on 2024/03/27.
+//
+
+import SwiftUI
+
+// MARK: - THEME MODEL
+
+struct Theme: Identifiable {
+  let id: Int
+  let themeName: String
+  let themeColor: Color
+}
